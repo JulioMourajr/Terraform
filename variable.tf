47988092ef0802bdf80d5e3a401bed1f1076417f
Julio Moura <juliomourajr@hotmail.com>
2024-11-07 21:33:14 -0300

    :sparkles: feat: Criando mais variaveis.

--- variable.tf
+++ variable.tf
@@ -3,3 +3,16 @@ variable "cidrvpc" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "iops" {
+  description = "IOPS for the RDS"
+  type        = number
+  default     = 3000
+
+}
+
+variable "allocated_storage" {
+  description = "Allocated storage for the RDS"
+  type        = number
+  default     = 400
+}
