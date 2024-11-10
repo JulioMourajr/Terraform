194a7174adcb16714453a8b6ffd8f8fb9066bb92
Julio Moura <juliomourajr@hotmail.com>
2024-11-10 16:27:40 -0300

    :sparkles: feat: Create policy and access for S3.

--- s3.tf
+++ s3.tf
@@ -7,3 +7,16 @@ resource "aws_s3_bucket" "ada-s3-julio" {
     Name = "ada-s3-julio"
   }
 }
+
+resource "aws_s3_bucket_policy" "ada-s3-julio-policy" {
+  bucket = aws_s3_bucket.ada-s3-julio.bucket
+  policy = local.policy
+}
+
+resource "aws_s3_bucket_public_access_block" "allow_access" {
+  bucket                  = aws_s3_bucket.ada-s3-julio.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  block_public_policy     = true
+}
