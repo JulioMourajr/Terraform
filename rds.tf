9249fa7eb1af1d35c2e507bee831864bd8977183
Julio Moura <juliomourajr@hotmail.com>
2024-11-10 03:32:32 -0300

    :sparkles: feat: add Security Group rules for RDS.

--- rds.tf
+++ rds.tf
@@ -3,6 +3,33 @@ resource "aws_db_subnet_group" "database-1" {
   subnet_ids = [aws_subnet.bd-a.id, aws_subnet.bd-b.id, aws_subnet.bd-c.id]
 }
 
+resource "aws_security_group" "database-sec" {
+  name        = "database-sec"
+  description = "Allow MySQL inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.ada_vpc.id
+
+  tags = {
+    Name = "database-sec"
+  }
+}
+
+resource "aws_security_group_rule" "database-sec-rule" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  security_group_id = aws_security_group.database-sec.id
+  cidr_blocks       = [aws_vpc.ada_vpc.cidr_block]
+}
+
+resource "aws_security_group_rule" "database-sec-rule-egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  security_group_id = aws_security_group.database-sec.id
+  cidr_blocks       = [aws_vpc.ada_vpc.cidr_block]
+}
 resource "aws_rds_cluster" "database-1" {
   cluster_identifier        = "database-1"
   availability_zones        = ["us-east-1a", "us-east-1b", "us-east-1c"]
@@ -14,4 +41,6 @@ resource "aws_rds_cluster" "database-1" {
   allocated_storage         = var.allocated_storage
   iops                      = var.iops
   master_username           = "admin"
+  master_password           = var.master_password
+  skip_final_snapshot = true
 }
